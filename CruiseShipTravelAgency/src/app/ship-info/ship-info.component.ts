3eaf1647829630925cb5cd6d70e0cdb0825917d3
George1044 <georgedaou_35@hotmail.com>
2020-12-09 21:18:22 +0200

    added carousel

--- CruiseShipTravelAgency/src/app/ship-info/ship-info.component.ts
+++ CruiseShipTravelAgency/src/app/ship-info/ship-info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {CruiseShipService} from "../services/cruise-ship.service";
 import {Room} from "../models/room";
 import {RoomApiResponse} from "../models/room-api-response";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-ship-info',
@@ -10,19 +11,29 @@ import {RoomApiResponse} from "../models/room-api-response";
 })
 export class ShipInfoComponent implements OnInit {
   rooms = [] ;
+  Floors =[];
+  NbFloors;
+  shipId;
 
   constructor(
+    private route: ActivatedRoute,
     private cruiseShipService : CruiseShipService
   ) { }
 
   ngOnInit(): void {
     window.scroll(0, 0);
-    this.cruiseShipService.getCruiseShipTravelPlanRooms(1, 1, '2020-11-30').subscribe(res => {
+    this.shipId = Number(this.route.snapshot.paramMap.get('id'))
+    this.cruiseShipService.getCruiseShip(this.shipId).subscribe(res =>{
+      this.NbFloors = res.data.cruiseship.shipNumberOfFloors
+      for(var i =0; i<this.NbFloors-2; i++)
+      this.Floors[i] = "../../assets/ship" + this.shipId + "DeckPlan" + "deckFloor" + (i+2);
+    });
+    this.cruiseShipService.getCruiseShipTravelPlanRooms(this.shipId, 1, '2020-11-30').subscribe(res => {
       this.rooms = res.data.rooms;
       console.log(res);
       this.rooms[0].roomStatus = "reserved";
       this.updateReserved(this.rooms[0].roomId);
-      for (var i = 0; i < this.rooms.length; i++) {
+      /*for (var i = 0; i < this.rooms.length; i++) {
         if (this.rooms[i].roomClass === "Royal Suite") {
           if(this.rooms[i].roomStatus === "free"){
           this.updateReserved(this.rooms[i].roomId);
@@ -31,7 +42,7 @@ export class ShipInfoComponent implements OnInit {
           console.log(this.rooms[i].roomClass)
           console.log(this.rooms[i].roomId);
         }
-      }
+      }*/
     })
 
   }
