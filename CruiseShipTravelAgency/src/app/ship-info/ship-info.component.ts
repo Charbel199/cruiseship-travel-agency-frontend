87dfa2897164a82137c70b17d326d7e1daaa0fad
George1044 <georgedaou_35@hotmail.com>
2020-12-18 03:48:50 +0200

    fixed about

--- CruiseShipTravelAgency/src/app/ship-info/ship-info.component.ts
+++ CruiseShipTravelAgency/src/app/ship-info/ship-info.component.ts
@@ -41,30 +41,8 @@ export class ShipInfoComponent implements OnInit {
         return a.roomId - b.roomId
       });
       console.log(this.rooms);
-      this.rooms[0].roomStatus = "reserved";
-      this.updateReserved(this.rooms[0].roomId);
-      /*for (var i = 0; i < this.rooms.length; i++) {
-        if (this.rooms[i].roomClass === "Royal Suite") {
-          if(this.rooms[i].roomStatus === "free"){
-          this.updateReserved(this.rooms[i].roomId);
-          this.rooms[i].roomStatus = "reserved";
-          break;}
-          console.log(this.rooms[i].roomClass)
-          console.log(this.rooms[i].roomId);
-        }
-      }*/
     })
 
-  }
-
-  updateReserved(id): void {
-    var room = document.getElementById("R" + id);
-    room.classList.add("reserved");
-  }
-
-  removeReserved(id): void {
-    var room = document.getElementById("R" + id);
-    room.classList.remove("reserved");
   }
 
   click(i): void {
