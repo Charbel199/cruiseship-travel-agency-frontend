2098dca4007e33a8d144cedb801cedd0659a1a99
charbel <charbel-boumaroun@outlook.com>
2020-12-18 03:00:54 +0200

    profile

--- CruiseShipTravelAgency/src/app/profile/profile.component.ts
+++ CruiseShipTravelAgency/src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CustomerService} from "../services/customer.service";
+import {ReservationService} from "../services/reservation.service";
+import {TravelPlanService} from "../services/travel-plan.service";
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  reservations;
+  travelPlans = [];
+  constructor(
+    private customerService: CustomerService,
+    private reservationService: ReservationService,
+    private travelPlanService: TravelPlanService
+  ) { }
 
   ngOnInit(): void {
+    this.reservationService.getAllReservations().subscribe( res => {
+      this.reservations = res.data.reservations;
+      console.log(this.reservations);
+      console.log(this.reservations[0].departureDate.substring(0,10));
+      for(var i =0;i<this.reservations.length;i++)
+      this.travelPlanService.getTravelPlan(this.reservations[i].reservationTravelPlanId).subscribe( res => {
+        this.travelPlans.push(res.data.travelplans);
+        console.log(this.travelPlans)
+      });
+    })
   }
-
+  substringDate(str: string){
+    return str.substring(0,10);
+  }
 }
